0cbaec217c29927cd4582611b9c922d1eb4190cb
Markus Tacker <m@coderbyheart.com>
2016-08-01 11:40:41 +0200

    chore(dependencies): fix tests for mocha 3

--- test/model-event.spec.js
+++ test/model-event.spec.js
@@ -7,11 +7,11 @@ const expect = require('chai').expect
 const Errors = require('rheactor-value-objects/errors')
 
 describe('ModelEvent', () => {
-    it('should have default values', (done) => {
-      let e = new ModelEvent()
-      expect(e.data).to.deep.equal({})
-      expect(e.createdAt).to.be.at.most(Date.now())
-      expect(e.createdBy).to.equal(undefined)
-      done()
-    })
+  it('should have default values', (done) => {
+    let e = new ModelEvent()
+    expect(e.data).to.deep.equal({})
+    expect(e.createdAt).to.be.at.most(Date.now())
+    expect(e.createdBy).to.equal(undefined)
+    done()
+  })
 })
